e6db65ef209bc36ba4493f5e1cebbdcb928408db
Heirin <eirinbjorknes@gmail.com>
2021-02-16 23:34:23 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
+import Input from "./Input/Input";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    translation: ""
+  }
+
+  tilRoever = event => {
+    let wordArray = event.target[0].value.split(" ");
+    let updatedArray = wordArray.map(word => {
+      let index = word.length-1;
+      let charArray = [...word];
+      let lastChar = charArray[index];
+      charArray.splice(index+1, 0, "o", lastChar);
+      let newWord = charArray.join("");
+      return newWord;
+    });
+    let newSentence = updatedArray.join(" ");
+    this.setState({translation: newSentence});
+    event.preventDefault();
+  }
+
+  fraRoever = event => {
+    let wordArray = event.target[0].value.split(" ");
+    let updatedArray = wordArray.map(word => {
+      let index = word.length-1;
+      let charArray = [...word];
+      charArray.splice(index-1, 2);
+      let newWord = charArray.join("");
+      return newWord;
+    });
+    let newSentence = updatedArray.join(" ");
+    this.setState({translation: newSentence});
+    event.preventDefault();
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <h1>Oversetter for røverspråk</h1>
+        <div>
+          <Input direction="til" submit={event=>this.tilRoever(event)}/>
+        </div>
+        <div>
+          <Input direction="fra" submit={event=>this.fraRoever(event)} />
+        </div>
+        <p>{this.state.translation}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Input/Input.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Input.css"
+
+function Input(props){
+    return <div className="input-cmp">
+        <form onSubmit={props.submit}>
+            <label>Skriv inn det du ønsker å oversette {props.direction} røverspråk</label>
+            <textarea cols="60" rows="10"></textarea>
+            <button type="submit">Oversett</button>
+        </form>
+    </div>;
+}
+
+export default Input;
